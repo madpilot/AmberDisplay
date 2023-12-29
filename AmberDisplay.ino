9780506b55d5398c378513656df9cd3e8601d60a
Myles Eftos <myles@madpilot.com.au>
2023-12-29 21:18:26 +0800

    Refactoring out general and feed in render

--- AmberDisplay.ino
+++ AmberDisplay.ino
@@ -27,7 +27,7 @@
 #define TIMER_DELAY 60000
 
 TFT_eSPI tft = TFT_eSPI(); 
-TFT_eSprite general_price_sprite = TFT_eSprite(&tft);
+TFT_eSprite price_sprite = TFT_eSprite(&tft);
 
 typedef enum {
   DESCRIPTOR_UNKNOWN,
@@ -50,13 +50,13 @@ typedef struct _channels {
   price_t controlled_load;
 } channels_t;
 
-void setClock() {
+void set_clock() {
   configTime(0, 0, "pool.ntp.org");
 
   Serial.print(F("Waiting for NTP time sync: "));
-  time_t nowSecs = time(nullptr);
+  time_t now_secs = time(nullptr);
   int attempts = 0;
-  while (nowSecs < 8 * 3600 * 2) {
+  while (now_secs < 8 * 3600 * 2) {
     delay(500);
     Serial.print(F("."));
     if(attempts % 2 == 0) {
@@ -66,20 +66,18 @@ void setClock() {
     }
     attempts++;
     yield();
-    nowSecs = time(nullptr);
+    now_secs = time(nullptr);
   }
   tft.fillRect(93, 125, 54, 3, TFT_AMBER_GREEN);
 
   Serial.println();
   struct tm timeinfo;
-  gmtime_r(&nowSecs, &timeinfo);
+  gmtime_r(&now_secs, &timeinfo);
   Serial.print(F("Current time: "));
   Serial.print(asctime(&timeinfo));
 }
 
-bool firstRun = true;
-unsigned long lastRun = 0;
-unsigned long renderLastRun = 0;
+unsigned long last_run = 0;
 bool connect()
 {
   Serial.print("Connecting to WiFi");
@@ -109,7 +107,7 @@ bool connect()
   {
     Serial.print("Connected to WiFi network with IP Address: ");
     Serial.println(WiFi.localIP());
-    setClock();  
+    set_clock();  
     return true;
   }
 }
@@ -186,19 +184,19 @@ channels_t fetch()
   }
 
   
-  unsigned long timeoutStart = millis();
+  unsigned long timeout_start = millis();
   char json[BUFFER_SIZE];
   memset((char *)&json, 0, BUFFER_SIZE);
   int i = 0;
   // Leave one character at the end, so the string is always terminated
-  while (i < BUFFER_SIZE - 1 && (http.connected() || http.available()) && ((millis() - timeoutStart) < 30000))
+  while (i < BUFFER_SIZE - 1 && (http.connected() || http.available()) && ((millis() - timeout_start) < 30000))
   {    
     if (http.available())
     {
       json[i++] = http.read();
       // Print out this character
       // We read something, reset the timeout counter
-      timeoutStart = millis();
+      timeout_start = millis();
     }
     else
     {
@@ -223,17 +221,17 @@ channels_t fetch()
     if (c.is<JsonObject>())
     {
       JsonObject channel = c.as<JsonObject>();
-      String channelType = channel["channelType"].as<String>();
+      String channel_type = channel["channelType"].as<String>();
       price_t *price = NULL;
-      if (channelType == String("general"))
+      if (channel_type == String("general"))
       {
         price = &(channels.general);
       } 
-      else if(channelType == String("feedIn")) 
+      else if(channel_type == String("feedIn")) 
       {
         price = &(channels.feed_in);
       } 
-      else if(channelType == String("controlledLoad")) 
+      else if(channel_type == String("controlledLoad")) 
       {
         price = &(channels.controlled_load);
       }
@@ -276,7 +274,7 @@ channels_t fetch()
   return channels;
 }
 
-void renderPrice(TFT_eSprite *sprite, float price, uint16_t text_colour, const unsigned short (*background)[32400]) {
+void render_price(TFT_eSprite *sprite, float price, uint16_t text_colour, const unsigned short (*background)[32400]) {
   sprite->loadFont(NotoSansBold36);
   sprite->setTextColor(text_colour);
   sprite->pushImage(0, 0, 240, 135, *background);
@@ -292,21 +290,97 @@ void renderPrice(TFT_eSprite *sprite, float price, uint16_t text_colour, const u
   sprite->unloadFont();
 }
 
+void render_general_price(TFT_eSprite *sprite, price_t *price) {
+  switch(price->descriptor) {
+    case DESCRIPTOR_SPIKE: {
+      Serial.printf("Price Spike!: %f\n", price->price);
+      render_price(sprite, price->price, TFT_WHITE, &price_spike);
+      break;
+    }
+    case DESCRIPTOR_HIGH: {
+      Serial.printf("High prices: %f\n", price->price);
+      render_price(sprite, price->price, TFT_AMBER_DARK_BLUE, &price_high);
+      break;
+    }
+    case DESCRIPTOR_NEUTRAL: {
+      Serial.printf("Average prices: %f\n", price->price);
+      render_price(sprite, price->price, TFT_AMBER_DARK_BLUE, &price_neutral);
+      break;
+    }
+    case DESCRIPTOR_LOW: {
+      Serial.printf("Low prices: %f\n", price->price);
+      render_price(sprite, price->price, TFT_AMBER_DARK_BLUE, &price_low);
+      break;
+    }
+    case DESCRIPTOR_VERY_LOW: {
+      Serial.printf("Very Low prices: %f\n", price->price);
+      render_price(sprite, price->price, TFT_AMBER_DARK_BLUE, &price_very_low);
+      break;
+    }
+    case DESCRIPTOR_EXTREMELY_LOW: {
+      Serial.printf("Extremely low prices: %f\n", price->price);
+      render_price(sprite, price->price, TFT_AMBER_DARK_BLUE, &price_extremely_low);
+      break;
+    }
+    default: {
+      Serial.printf("Unknown Price\n");
+    }
+  }
+}
+
+void render_feed_in(TFT_eSprite *sprite, price_t *price) {
+  switch(price->descriptor) {
+    case DESCRIPTOR_SPIKE: {
+      Serial.printf("Price Spike!: %f\n", price->price);
+      render_price(sprite, -1 * price->price, TFT_AMBER_DARK_BLUE, &price_low);
+      break;
+    }
+    case DESCRIPTOR_HIGH: {
+      Serial.printf("High prices: %f\n", price->price);
+      render_price(sprite, -1 * price->price, TFT_AMBER_DARK_BLUE, &price_low);
+      break;
+    }
+    case DESCRIPTOR_NEUTRAL: {
+      Serial.printf("Average prices: %f\n", price->price);
+      render_price(sprite, -1 * price->price, TFT_AMBER_DARK_BLUE, &price_low);
+      break;
+    }
+    case DESCRIPTOR_LOW: {
+      Serial.printf("Low prices: %f\n", price->price);
+      render_price(sprite, -1 * price->price, TFT_AMBER_DARK_BLUE, &price_low);
+      break;
+    }
+    case DESCRIPTOR_VERY_LOW: {
+      Serial.printf("Very Low prices: %f\n", price->price);
+      render_price(sprite, -1 * price->price, TFT_AMBER_DARK_BLUE, &price_low);
+      break;
+    }
+    case DESCRIPTOR_EXTREMELY_LOW: {
+      Serial.printf("Extremely low prices: %f\n", price->price);
+      render_price(sprite, -1 * price->price, TFT_AMBER_RED, &price_low);
+      break;
+    }
+    default: {
+      Serial.printf("Unknown Price\n");
+    }
+  }
+}
+
 void setup()
 {
   Serial.begin(115200);
   // Force a run on startup
-  lastRun = TIMER_DELAY;
+  last_run = TIMER_DELAY;
 
   tft.init();
   tft.setRotation(1);
   tft.setSwapBytes(true);
   tft.pushImage(0, 0, 240, 135, splash_image);
-  general_price_sprite.createSprite(tft.width(), tft.height());
-  general_price_sprite.setTextSize(3);
-  general_price_sprite.setCursor(0, 0); 
-  general_price_sprite.setTextDatum(MC_DATUM);
-  general_price_sprite.setSwapBytes(true);
+  price_sprite.createSprite(tft.width(), tft.height());
+  price_sprite.setTextSize(3);
+  price_sprite.setCursor(0, 0); 
+  price_sprite.setTextDatum(MC_DATUM);
+  price_sprite.setSwapBytes(true);
 
   WiFi.begin(WIFI_SSID, WIFI_PASSKEY);
 }
@@ -323,49 +397,13 @@ void loop()
   }
 
 
-  if ((millis() - lastRun) > TIMER_DELAY)
+  if ((millis() - last_run) > TIMER_DELAY)
   {
     Serial.println("Checking the price");
     channels_t channels = fetch();
-    price_t *current = &(channels.general);
-    firstRun = false;
-
-    switch(current->descriptor) {
-      case DESCRIPTOR_SPIKE: {
-        Serial.printf("Price Spike!: %f\n", current->price);
-        renderPrice(&general_price_sprite, current->price, TFT_WHITE, &price_spike);
-        break;
-      }
-      case DESCRIPTOR_HIGH: {
-        Serial.printf("High prices: %f\n", current->price);
-        renderPrice(&general_price_sprite, current->price, TFT_AMBER_DARK_BLUE, &price_high);
-        break;
-      }
-      case DESCRIPTOR_NEUTRAL: {
-        Serial.printf("Average prices: %f\n", current->price);
-        renderPrice(&general_price_sprite, current->price, TFT_AMBER_DARK_BLUE, &price_neutral);
-        break;
-      }
-      case DESCRIPTOR_LOW: {
-        Serial.printf("Low prices: %f\n", current->price);
-        renderPrice(&general_price_sprite, current->price, TFT_AMBER_DARK_BLUE, &price_low);
-        break;
-      }
-      case DESCRIPTOR_VERY_LOW: {
-        Serial.printf("Very Low prices: %f\n", current->price);
-        renderPrice(&general_price_sprite, current->price, TFT_AMBER_DARK_BLUE, &price_very_low);
-        break;
-      }
-      case DESCRIPTOR_EXTREMELY_LOW: {
-        Serial.printf("Extremely low prices: %f\n", current->price);
-        renderPrice(&general_price_sprite, current->price, TFT_AMBER_DARK_BLUE, &price_extremely_low);
-        break;
-      }
-      default: {
-        Serial.printf("Unknown Price\n");
-      }
-    }
-    general_price_sprite.pushSprite(tft.width() / 2 - general_price_sprite.width() / 2, 0);
-    lastRun = millis();
+    render_general_price(&price_sprite, &(channels.general));
+    //render_feed_in(&price_sprite, &(channels.feed_in));
+    price_sprite.pushSprite(tft.width() / 2 - price_sprite.width() / 2, 0);
+    last_run = millis();
   }
 }
